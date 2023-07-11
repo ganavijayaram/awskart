1b8c9ce135c7501340ef69990fccd5abb0f4782f
ganavijayaram <jayaram.g@northeastern.edu>
2023-07-11 16:08:39 -0400

    creating dynamoDB for product microservice

--- code/lib/code-stack.ts
+++ code/lib/code-stack.ts
@@ -1,4 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
+import { RemovalPolicy } from 'aws-cdk-lib';
+//Creating the dynamo table
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +9,19 @@ export class CodeStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CodeQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+   //creating dynamo table
+   //scope, id, properties
+   const productTable = new Table(this, 'product', {
+    //partiton key
+    partitionKey: {
+      name: 'id',
+      type: AttributeType.STRING
+    },
+    //name of the table
+    tableName: 'product',
+    //to destroy when we run cdk destroy, else even if run cdk destroy, it will not destroy if we dont mention destroy
+    removalPolicy: RemovalPolicy.DESTROY,
+    billingMode: BillingMode.PAY_PER_REQUEST
+   })
   }
 }
