f9e39fb6dc2ded52bd0af8d19d08eec25bffa58b
ganavijayaram <jayaram.g@northeastern.edu>
2023-07-11 18:34:42 -0400

    Creating lamda function with necessary properties

--- code/lib/code-stack.ts
+++ code/lib/code-stack.ts
@@ -2,7 +2,10 @@ import * as cdk from 'aws-cdk-lib';
 import { RemovalPolicy } from 'aws-cdk-lib';
 //Creating the dynamo table
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
+import { join } from 'path';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CodeStack extends cdk.Stack {
@@ -23,5 +26,26 @@ export class CodeStack extends cdk.Stack {
     removalPolicy: RemovalPolicy.DESTROY,
     billingMode: BillingMode.PAY_PER_REQUEST
    })
+
+   //Defining propertites for the node 
+   const NodejsFunctionProps: NodejsFunctionProps = {
+    bundling: {
+      //in this function if we need extrernal libraries we say aws-sdk
+      externalModules: [
+        'aws-sdk'
+      ]
+    },
+    environment: {
+      PRIMARY_KEY: 'id',
+      DYNAMO_TABLE_NAME: productTable.tableName
+    },
+    runtime: Runtime.NODEJS_18_X
+   }
+
+   //creating lambda function using the nodejs for bundling and packaging
+   const productFunction = new NodejsFunction(this, 'productLambdaFunction', {
+    entry: join(__dirname, `/../src/product/index.js`)
+   })
+
   }
 }
