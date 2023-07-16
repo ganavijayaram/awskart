6806dce168580fea6327921644d55b4b8a6274ba
ganavijayaram <jayaram.g@northeastern.edu>
2023-07-15 21:38:42 -0400

    getProduct function

--- code/src/product/index.js
+++ code/src/product/index.js
@@ -1,4 +1,7 @@
-const { get } = require("http");
+//Using thr EC6+
+import { GetItemCommand } from ("@aws-sdk/client-dynamodb");
+import { marshall, unmarshall } from require("@aws-sdk/util-dynamodb");
+import { ddbClient } from require("./ddbClient");
 
 exports.handler = async function(event) {
     console.log("result:", JSON.stringify(event, undefined, 2));
@@ -11,6 +14,30 @@ exports.handler = async function(event) {
             else {
                 body = await getAllProducts(); //GET /product
             }
+    }
+
+    const getProduct = async(productId) => {
+        console.log("getProduct")
+
+        try {
+            //Setting the parameters to query the table 
+            const params = {
+                TableName: process.env.DYNAMO_TABLE_NAME,
+                Key: marshall({id: productId})
+            }
+            //Querying the table and waiting till we get the response using the DynamoDB API's 
+            const {Item} = await ddbClient.send(new GetItemCommand(params))
+
+            console.log(Item)
+
+            //If the Item is not null, we will unmarshall it else we will send an empty json object
+            return {Item} ? unmarshall(Item) : {}
+        }
+        catch (e){
+            console.error(e)
+            throw e
+
+        }
     }
     return {
         statusCode: 200,
