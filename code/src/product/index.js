18f4d17e4befcb7d960cd629fa883368ea60a0f7
ganavijayaram <jayaram.g@northeastern.edu>
2023-07-15 20:19:34 -0400

    handling the GET metod triggered from the API Gateway

--- code/src/product/index.js
+++ code/src/product/index.js
@@ -1,5 +1,17 @@
+const { get } = require("http");
+
 exports.handler = async function(event) {
     console.log("result:", JSON.stringify(event, undefined, 2));
+    //Using switch to respond to various methods from the API gateway
+    switch(event.httpMethod) {
+        case "GET":
+            if(event.pathParameters != null) {
+                body = await getProduct(event.pathParameters.id); //GET /product/{id}
+            }
+            else {
+                body = await getAllProducts(); //GET /product
+            }
+    }
     return {
         statusCode: 200,
         headers: {"Content-Type": "text/plain"},
